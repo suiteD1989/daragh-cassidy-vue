396f91e59a403510c5f8eb13aee365d57d484c90
suiteD1989 <daraghcassidy89@gmail.com>
2020-02-17 21:19:47 +0000

    added meta data to blog

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,6 +1,6 @@
 echo "Running staging build..."
 
-npm run build
+npm run staging
 
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
--- pages/blog/_slug/index.vue
+++ pages/blog/_slug/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Navigation/>
+    <Navigation :navLinks="this.navItems"/>
     <article class="container blog-post content-piece">
       {{ page.fields.body }}
     </article>
@@ -13,19 +13,66 @@
 import Navigation from '~/components/navigation.vue';
 import Footer from '~/components/footer.vue';
 
+import robots from '~/mixins/robots.js';
+
 import {createClient} from '~/plugins/contentful';
 const contentfulClient = createClient();
 
 export default {
   name: 'index',
+  mixins: [robots],
   components: {
     Navigation,
     Footer
   },
+  data () {
+    return {
+      navItems: [
+        {
+          title: 'Home',
+          config: 'home',
+          link: '/'
+        },
+        {
+          title: 'Blog',
+          config: 'blog',
+          link: '/blog'
+        }
+      ],
+      pageData: Object
+    }
+  },
+  head () {
+    return {
+      title: this.page.fields.title,
+      meta: [
+        {
+          hid: 'robots', 
+          name: 'robots', 
+          content: this.setRobotsTag(process.env.SET_ENVIRONMENT)
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: this.page.fields.title
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.page.fields.description
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: this.page.fields.description
+        }
+      ]
+    }
+  },
   asyncData ({ env, params }) {
     return contentfulClient.getEntries({
         'content_type': 'blogPost',
-        'fields.slug': params.id
+        'fields.slug': params.slug
     })
     .then(page => {
         return {
