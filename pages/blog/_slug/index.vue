251d59f03152715f2a9950191d070435ee2c4892
suiteD1989 <daraghcassidy89@gmail.com>
2020-02-17 21:38:56 +0000

    blog styling

--- pages/blog/_slug/index.vue
+++ pages/blog/_slug/index.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
     <Navigation :navLinks="this.navItems"/>
-    <article class="container blog-post content-piece">
-      {{ page.fields.body }}
+    <article class="container-fluid blog-post content-piece">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="intro">
+              <h1>{{ page.fields.title }}</h1>
+              <span>{{ page.fields.author.fields.name }}</span>
+            </div>
+            <client-only>
+              <img 
+              :data-src="setAutoWidth(page.fields.heroImage.fields.file.url)"
+              :alt="page.fields.heroImage.fields.description"
+              class="lazyload header-img">
+            </client-only>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <p>
+              {{ page.fields.body }}
+            </p>
+          </div>
+        </div>
+      </div>
     </article>
     <Footer/>
   </div>
@@ -14,13 +36,14 @@ import Navigation from '~/components/navigation.vue';
 import Footer from '~/components/footer.vue';
 
 import robots from '~/mixins/robots.js';
+import optimise from '~/mixins/optimise.js';
 
 import {createClient} from '~/plugins/contentful';
 const contentfulClient = createClient();
 
 export default {
   name: 'index',
-  mixins: [robots],
+  mixins: [robots, optimise],
   components: {
     Navigation,
     Footer
@@ -88,5 +111,25 @@ export default {
 <style lang="scss" scoped>
   .blog-post {
     color: black;
+    $blue-1: #598381;
+    $blue-2: #177E89;
+    $blue-3: #08605F;
+
+    .intro {
+      h1 {
+        font-size: 2em;
+        color: $blue-2;
+        text-transform: uppercase;
+      }
+      span {
+        color: $blue-1;
+        font-size: .9em;
+        padding-bottom: 1em;
+        display: block;
+      }
+    }
+    .header-img {
+      width: 100%;
+    }
   }
 </style>
