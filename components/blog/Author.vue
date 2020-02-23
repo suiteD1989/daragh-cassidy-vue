ca4899606ec958d432a0c90fbb9e17fca9e28012
suiteD1989 <daraghcassidy89@gmail.com>
2020-02-23 23:45:39 +0000

    mobile stying

--- components/blog/Author.vue
+++ components/blog/Author.vue
@@ -3,10 +3,10 @@
         <div class="col-12">
             <h2>About Daragh</h2>
         </div>
-        <div class="col-3 my-auto">
+        <div class="col-md-3 col-lg-3 col-sm-12 my-auto">
             <img :data-src="this.details.fields.image.fields.file.url" class="lazyload profile">
         </div>
-        <div class="col-9">
+        <div class="col-md-9 col-lg-9 col-sm-12">
             <div class="col-12">
                 <p>{{ this.details.fields.shortBio }}</p>
             </div>  
